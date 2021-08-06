01a62784217e5681d63da99a2b1296ddcdab948d
Irfan <ik7417990@gmail.com>
2021-08-06 13:19:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './style.css';
+// import TodoList from "./TodoList";
+import Todo from "./Todo"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TodoList/> */}
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
